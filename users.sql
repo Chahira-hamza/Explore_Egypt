6a11ca9b672e785c4379dec51613a148e5c362aa
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-03-24 20:32:26 +0200

    sql table for users

---
+++ users.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `users` (
+  `username` varchar(20) NOT NULL,
+  `password_` varchar(20) NOT NULL,
+  `user_type` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;

143802044893ffddce7a4e0309264c7bcb8b50dd
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-08 23:40:12 +0200

    Update and rename Model.java to Model/Model.java

--- Model/Model.java
+++ Model/Model.java
@@ -5,12 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Properties;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author chahira

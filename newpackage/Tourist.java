a9cd8c74c2d6048d70b763c1364feffb3e035088
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-09 14:41:29 -0700

    Add files via upload

---
+++ newpackage/Tourist.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author chahira
+ */
+public class Tourist extends User {
+    
+    protected int mobileNum;
+    protected String email;
+    protected String country;
+    protected String L1;
+    protected String L2;
+    protected String L3;
+
+    public int getMobileNum() {
+        return mobileNum;
+    }
+
+    public void setMobileNum(int mobileNum) {
+        this.mobileNum = mobileNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getL1() {
+        return L1;
+    }
+
+    public void setL1(String L1) {
+        this.L1 = L1;
+    }
+
+    public String getL2() {
+        return L2;
+    }
+
+    public void setL2(String L2) {
+        this.L2 = L2;
+    }
+
+    public String getL3() {
+        return L3;
+    }
+
+    public void setL3(String L3) {
+        this.L3 = L3;
+    }
+    
+
+}

f008a28c1dbc396728d66a40f776f1ad43fc2ee5
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-10 03:13:33 +0200

    Update tourists.sql

--- Queries/tourists.sql
+++ Queries/tourists.sql
@@ -1,10 +1,11 @@
-CREATE TABLE `tourists` (
+CREATE TABLE `tourguide` (
   `mobile_num` int(15) NOT NULL,
   `email` varchar(20) NOT NULL,
   `country` varchar(30) NOT NULL,
-  `L1` varchar(11) NOT NULL, /* The tourist first language  */
-  `L2` varchar(11) ,		 /* The tourist second language */
-  `L3` varchar(11) ,		 /* The tourist third language  */
-  `tourist_ID` int(11) NOT NULL,
-  PRIMARY KEY (`tourist_ID`)
-)
+  `L1` varchar(11) NOT NULL,
+  `L2` varchar(11) DEFAULT NULL,
+  `L3` varchar(11) DEFAULT NULL,
+  `tourguide_ID` int(11) NOT NULL,
+  KEY `tourguide_ID` (`tourguide_ID`),
+  CONSTRAINT `tourguide_ibfk_1` FOREIGN KEY (`tourguide_ID`) REFERENCES `users` (`user_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

0362d71656e9ccec32bd9c4dae93426dfd48a705
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-10 03:14:15 +0200

    Update sites_rating.sql

--- Queries/sites_rating.sql
+++ Queries/sites_rating.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `sites_rating`(
-`user_ID` int(11) NOT NULL,
-`site_name` varchar(20) NOT NULL,
-`rating` int(1),
-PRIMARY KEY (`user_ID`)
-)
+CREATE TABLE `sites_rating` (
+  `user_ID` int(11) NOT NULL,
+  `site_name` varchar(20) NOT NULL,
+  `rating` int(1) DEFAULT NULL,
+  PRIMARY KEY (`user_ID`),
+  CONSTRAINT `sites_rating_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
